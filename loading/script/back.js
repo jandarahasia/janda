c963381038d9652763f6017f2edebc46327fe562
jandarahasia <85703474+jandarahasia@users.noreply.github.com>
2021-12-03 04:03:32 +0700

    Delete back.js

--- loading/script/back.js
+++
@@ -1,7 +0,0 @@
-  if (typeof history.pushState === "function") {
-	history.pushState("back", null, null);
-	window.onpopstate = function() {
-	    history.pushState("back", null, null);
-	      	window.location.href = "https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=jandarahasia";
-	    }
-  	};
